808da942480215b1e8d5f392bb96b818a553d1e6
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-06-18 11:01:15 -0400

    adding cat service

--- src/app/nomenclator-modules/cat-docent/services/cat-docent.service.ts
+++ src/app/nomenclator-modules/cat-docent/services/cat-docent.service.ts
@@ -3,12 +3,74 @@ import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 
 import { ApiResponse } from 'src/app/core/models/api-response.model';
-// import { CatDocent } from '../models/'
+import { CatDocent } from '../models/cat-docent.model';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CatDocentService {
+  private apiEndpoint = `${environment.apiUrl}categoria_docente`;
+  private defaultFilter: any = {};
 
-  constructor() { }
+  private defaultSortColumn: string = 'description';
+  private defaultSortDirection: string = 'desc';
+  private defaultPage: number = 0;
+  private defaultPageSize: number = 10;
+
+  constructor(private http: HttpClient) {}
+
+  getCatDocent(filter: any, sortColumn: string, sortDirection: string, page: number, pageSize: number): Observable<ApiResponse<CatDocent>> {
+    this.defaultFilter = filter;
+    this.defaultSortColumn = sortColumn;
+    this.defaultSortDirection = sortDirection;
+    this.defaultPage = page;
+    this.defaultPageSize = pageSize;
+
+    const queryParams = this.formatQueryParams(filter, sortColumn, sortDirection, page, pageSize);
+    return this.http.get<ApiResponse<CatDocent>>(this.apiEndpoint + queryParams);
+  }
+
+  private formatQueryParams(filters?: any, sortColumn?: string, sortDirection?: string, pageIndex?: number, pageSize?: number): string {
+    let queryParams = '';
+    if (filters) {
+      for (const property in filters) {
+        queryParams += queryParams.length > 0 ? '&' : '?';
+        queryParams += `${property}=${filters[property]}`;
+      }
+    }
+
+    if (sortColumn) {
+      let ordering = '';
+
+      if (sortDirection === 'desc') {
+        ordering = '-';
+      }
+      ordering += sortColumn;
+      queryParams += queryParams.length > 0 ? '&' : '?';
+      queryParams += `ordering=${ordering}`;
+    }
+
+    if (pageIndex !== undefined) {
+      queryParams += queryParams.length > 0 ? '&' : '?';
+      queryParams += `page=${pageIndex}`;
+    }
+    if (pageSize !== undefined) {
+      queryParams += queryParams.length > 0 ? '&' : '?';
+      queryParams += `page_size=${pageSize}`;
+    }
+    return queryParams;
+  }
+
+  createCatDocent(data: CatDocent): Observable<CatDocent> {
+    return this.http.post<any>(`${this.apiEndpoint}/`, data);
+  }
+
+  editCatDocent(data: CatDocent): Observable<CatDocent> {
+    return this.http.put<CatDocent>(`${this.apiEndpoint}/${data.id}/`, data)
+  }
+
+  deleteCatDocent(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiEndpoint}/${id}/`);
+  }
 }
