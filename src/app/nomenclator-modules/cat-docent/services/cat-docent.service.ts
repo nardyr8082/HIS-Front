4f9babb1b8fd6c139210871c80b8f28291c9dabb
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-06-18 09:56:04 -0400

    adding a service for docent category

--- src/app/nomenclator-modules/cat-docent/services/cat-docent.service.ts
+++ src/app/nomenclator-modules/cat-docent/services/cat-docent.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+import { ApiResponse } from 'src/app/core/models/api-response.model';
+// import { CatDocent } from '../models/'
 
 @Injectable({
   providedIn: 'root'
