5775707ba65d0049d9902164ec4a4d46053fd00c
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-06-18 11:24:21 -0400

    adding docent form component

--- src/app/nomenclator-modules/cat-docent/components/cat-docent-form/cat-docent-form.component.ts
+++ src/app/nomenclator-modules/cat-docent/components/cat-docent-form/cat-docent-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EventEmitter } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-cat-docent-form',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cat-docent-form.component.scss']
 })
 export class CatDocentFormComponent implements OnInit {
+  @Output() create: EventEmitter<any> = new EventEmitter();
+  @Output() edit: EventEmitter<any> = new EventEmitter();
+  
+  catDocentForm: FormGroup;
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<CatDocentFormComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit(): void {
+    this.buildForm();
   }
 
+  buildForm() {
+    this.catDocentForm = new FormGroup({
+      description: new FormControl(this.data.catDocent ? this.data.catDocent.description : '', Validators.required),
+    })
+  }
+
+  onSubmit(data) {
+    this.data.catDocent ? this.edit.emit(data) : this.create.emit(data);
+    this.dialogRef.close();
+  }
+
+  onCancel() {
+    this.dialogRef.close();
+  }
 }
