a76de30e66ed9829223c65c0672c4424d6a5551a
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-06-18 01:29:38 -0400

    creating cat-docent-page component

---
+++ src/app/nomenclator-modules/cat-docent/pages/cat-docent-page/cat-docent-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cat-docent-page',
+  templateUrl: './cat-docent-page.component.html',
+  styleUrls: ['./cat-docent-page.component.scss']
+})
+export class CatDocentPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
