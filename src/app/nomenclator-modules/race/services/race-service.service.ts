b3c4e7ffefcf6f279451af2cc993a02dac4a3390
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-06-19 10:10:33 -0400

    adding race service

---
+++ src/app/nomenclator-modules/race/services/race-service.service.ts
@@ -0,0 +1,81 @@
+import { environment } from '../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { ApiResponse } from '../../../core/models/api-response.model';
+import { Race } from '../models/race.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RaceService {
+  private apiEndpoint = `${environment.apiUrl}raza`;
+  private defaultFilter: any = {};
+
+  private defaultSortColumn: string = 'descripcion';
+
+  private defaultSortDirection: string = 'desc';
+
+  private defaultPage: number = 0;
+
+  private defaultPageSize: number = 10;
+
+  constructor(private http: HttpClient) { }
+
+  getRace(filter: any, sortColumn: string, sortDirection: string, page: number, pageSize: number): Observable<ApiResponse<Race>> {
+    this.defaultFilter = filter;
+    this.defaultSortColumn = sortColumn;
+    this.defaultSortDirection = sortDirection;
+    this.defaultPage = page;
+    this.defaultPageSize = pageSize;
+
+    const queryParams = this.formatQueryParams(filter, sortColumn, sortDirection, page, pageSize);
+    return this.http.get<ApiResponse<Race>>(this.apiEndpoint + queryParams);
+  }
+
+  private formatQueryParams(filters?: any, sortColumn?: string, sortDirection?: string, pageIndex?: number, pageSize?: number): string {
+    let queryParams = '';
+
+    if (filters) {
+      for (const property in filters) {
+        queryParams += queryParams.length > 0 ? '&' : '?';
+        queryParams += `${property}=${filters[property]}`;
+      }
+    }
+
+    if (sortColumn) {
+      let ordering = '';
+
+      if (sortDirection === 'desc') {
+        ordering = '-';
+      }
+      ordering += sortColumn;
+      queryParams += queryParams.length > 0 ? '&' : '?';
+      queryParams += `ordering=${ordering}`;
+    }
+
+    if (pageIndex !== undefined) {
+      queryParams += queryParams.length > 0 ? '&' : '?';
+      queryParams += `page=${pageIndex}`;
+    }
+
+    if (pageSize !== undefined) {
+      queryParams += queryParams.length > 0 ? '&' : '?';
+      queryParams += `page_size=${pageSize}`;
+    }
+
+    return queryParams;
+  }
+
+  createRace(data: Race): Observable<Race> {
+    return this.http.post<any>(`${this.apiEndpoint}/`, data);
+  }
+
+  editRace(data: Race): Observable<Race> {
+    return this.http.put<Race>(`${this.apiEndpoint}/${data.id}/`, data);
+  }
+
+  deleteRace(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiEndpoint}/${id}/`);
+  }
+}
