dc16db794f29321dd9b4faed82e792e8f5037d24
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-06-20 11:46:02 -0400

    adding route to workstation module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -104,6 +104,11 @@ const routes: Routes = [
         loadChildren: () => import('./security-module/user/user.module').then((m) => m.UserModule),
         canActivate: [BackendGuard],
       },
+      {
+        path: 'work-station',
+        loadChildren: () => import('./structure-modules/work-station/work-station.module').then((m) => m.WorkStationModule),
+        canActivate: [BackendGuard],
+      },
     ],
   },
   {
