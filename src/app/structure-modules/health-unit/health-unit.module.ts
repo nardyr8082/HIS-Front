623d172c25c8bd8c10a2bd2048786bba73af7a55
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-06-21 19:39:50 -0400

    adding health unit module

--- src/app/structure-modules/health-unit/health-unit.module.ts
+++ src/app/structure-modules/health-unit/health-unit.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
-import { HealtUnitFormComponent } from './components/healt-unit-form/healt-unit-form.component';
+import { HealthUnitFormComponent } from './components/health-unit-form/health-unit-form.component';
 import { HealthUnitPageComponent } from './pages/health-unit-page/health-unit-page.component';
 
 import { HealthUnitRoutingModule } from './health-unit-routing.module';
 
 
 @NgModule({
-  declarations: [HealtUnitFormComponent, HealthUnitPageComponent],
+  declarations: [HealthUnitFormComponent, HealthUnitPageComponent],
   imports: [
     CommonModule,
     HealthUnitRoutingModule,
