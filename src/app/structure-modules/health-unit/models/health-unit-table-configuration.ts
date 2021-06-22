4954c5e8938276b8ecf8e73675e98bf53525e934
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-06-22 15:01:19 -0400

    fixing model and table config

--- src/app/structure-modules/health-unit/models/health-unit-table-configuration.ts
+++ src/app/structure-modules/health-unit/models/health-unit-table-configuration.ts
@@ -2,23 +2,23 @@ import { DEFAULT_PAGINATION_SIZE } from '../../../core/models/api-response.model
 
 export const HEALTH_UNIT_TABLE_CONFIGURATION = {
   paginationSize: DEFAULT_PAGINATION_SIZE,
-  displayedColumns: ['name', 'editable', 'permissions_string'],
+  displayedColumns: ['nombre', 'levels_string', 'direccion'],
   columnsName: ['Nombre', 'Nivel', 'Direccion'],
   tableFilters: [
     {
-      name: 'name',
+      name: 'nombre',
       type: 'text',
       title: 'Descripción',
+    },
+    {
+      name: 'levels_string',
+      type: 'text',
+      title: 'Nivel',
     },
     {
       name: 'direccion',
       type: 'text',
       title: 'Direccion',
-    },
-    {
-      name: 'nivel',
-      type: 'text',
-      title: 'Nivel',
     },
   ],
 };
