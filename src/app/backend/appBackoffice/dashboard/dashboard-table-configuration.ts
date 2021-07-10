18d0e2b3219cbbf429628f0466c3a879cbf65eb5
Marisleidys Martín <marisleidysm@gmail.com>
2021-07-10 11:48:38 -0400

    Cambio de gráficos por tabla en Dashboard

---
+++ src/app/backend/appBackoffice/dashboard/dashboard-table-configuration.ts
@@ -0,0 +1,7 @@
+import { DEFAULT_PAGINATION_SIZE } from '../../../core/models/api-response.model';
+
+export const TABLE_CONFIGURATION = {
+    paginationSize: DEFAULT_PAGINATION_SIZE,
+    displayedColumns: ['name', 'count'],
+    columnsName: ['Elemento', 'Cantidad'],
+}
--- src/app/backend/appBackoffice/dashboard/dashboard.component.ts
+++ src/app/backend/appBackoffice/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardService } from '../../services/dashboard/dashboard.service';
+import { TABLE_CONFIGURATION } from './dashboard-table-configuration';
+import {  DEFAULT_PAGE_SIZE } from 'src/app/core/models/api-response.model';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,13 +10,25 @@ import { DashboardService } from '../../services/dashboard/dashboard.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  data: any = {};
+  data: any = [];
+  dataCount = 0;
+  configuration = TABLE_CONFIGURATION;
+  page = 1;
+  pageSize = DEFAULT_PAGE_SIZE;
+  loading = false;
   constructor(public dashboardService: DashboardService) {}
 
   ngOnInit(): void {
     this.dashboardService.getDashboardData().subscribe( resp => {
-      this.data = resp;
+      this.data = resp.dataCount;
+      console.log(this.data);
     });
+  }
+
+  onChangePage(page: PageEvent) {
+    this.page = page.pageIndex + 1;
+    this.pageSize = page.pageSize;
+  
   }
 
 }
