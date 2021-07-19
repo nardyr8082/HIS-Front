ae98547cbb25ab3c2ebd5145478ca88fa99cb41f
Marisleidys Martín <marisleidysm@gmail.com>
2021-07-18 22:44:59 -0400

    Arreglando error en Indicador Variable

--- src/app/indicator-module/variable-indicator/components/variable-indicator-form/variable-indicator-form.component.spec.ts
+++ src/app/indicator-module/variable-indicator/components/variable-indicator-form/variable-indicator-form.component.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SubcategoryFormComponent } from './subcategory-form.component';
+import { VariableIndicatorFormComponent } from './variable-indicator-form.component';
 
-describe('SubcategoryFormComponent', () => {
-  let component: SubcategoryFormComponent;
-  let fixture: ComponentFixture<SubcategoryFormComponent>;
+describe('VariableIndicadtorFormComponent', () => {
+  let component: VariableIndicatorFormComponent;
+  let fixture: ComponentFixture<VariableIndicatorFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SubcategoryFormComponent],
+      declarations: [VariableIndicatorFormComponent],
     }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SubcategoryFormComponent);
+    fixture = TestBed.createComponent(VariableIndicatorFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
